93dd2d474d0a474560fbe6a9c340bcfe6d8de631
Nancy-Klemm <nancy.klemm@dci.education>
2023-02-22 16:34:56 +0100

    update6

--- index.js
+++ index.js
@@ -1,66 +1,77 @@
 ///////// Pokemon Lite /////////
 
-
 class Pokemon {
-    constructor(name, health, magic){
+    constructor(name, health, magic) {
         this.name = name;
         this.health = health;
         this.magic = magic;
         this.skills = [];
     }
 
-    learnAttackSkill(attack){
-        const add = this.skills.push(attack); 
+    learnAttackSkill(attack) {
+        const add = this.skills.push(attack);
         return this.skills;
     }
 
-    showStatus(){
-        if(this.health < 1){
-            return `${this.name} was defeated.`
+    showStatus() {
+        if (this.health < 1) {
+            return `${this.name} was defeated.`;
         } else {
-
-        return `${this.name} status
+            return `${this.name} status
         health: ${this.health}
-        magic: ${this.magic}`
+        magic: ${this.magic}`;
         }
     }
-    attack(index, target){
-        // Magie Prüfung
-        if(this.magic >= this.skills.kT){
-            this.magic -= this.skills.kT
+
+    attack(index, target) {
         // Kampfunfähigkeitsprüfung
-        } else if(target.health <= 0){
-            return `${this.name} defeated ${target.name}. ${this.name} won the Battle!` 
-        // Prüfung der Ausführbarkeit der Attacke
-        } else if (target.health >= 0) {
-           target.health -= this.skills[index].damage;
+        // // Wenn die gegnerische Gesundheit größer 0 ist, dann bekommt der Gegner Damage
+        if (target.health > 0 && this.health > 0) {
+            // // Magieprüfung
+            // // Wenn die Magie größer oder gleich die Magic Poins sind, dann ziehe die Magic Points von der Magie ab
+            if (this.magic >= this.skills[index].magicPoints) {
+                this.magic -= this.skills[index].magicPoints;
+            }
+            // // Wenn die Magie kleiner als die Magic Points ist, dann ist nicht genuf Magie für den Angriff übrig
+            else if (this.magic < this.skills[index].magicPoints) {
+                console.log(`not enough magic, cannot launch attack!`);
+            } else if (this.health <= 0) {
+                console.log(`${this.name} was already defeated!`);
+            }
+            target.health -= this.skills[index].damage;
             return `${this.name} launched skill ${this.skills[index].name} successfully
-            ${target.name} has ${target.health} health and got ${this.skills[index].damage} damage`
-        // Prüfung der KT Kosten
-        } else if (this.magic <= this.skills[index].kT){
-           return `not enough magic, cannot launch attack!`}
+            ${target.name} got ${this.skills[index].damage} damage`;
+            // // Wenn die gegnerische Gesundheit kleiner oder gleich 0 ist, dann hat das angreifende Pokemon gewonnen
+        } else if (target.health <= 0) {
+            return `${this.name} defeated ${target.name}. ${this.name} won the Battle!`;
         }
-       
-    getMagic(){
-           const newMagic = this.magic + 20;
-            return `${this.name} got 20 magic back
-            ${this.name} has ${newMagic} magic`
+
+        // if (this.magic >= this.skills[index].magicPoints) {
+        //     this.magic -= this.skills[index].magicPoints;
+        //     return `${this.name} has used ${this.skills[index].magicPoints} Magic Points and has enough magic left`;
+        //     }
+        //     // // Wenn die Magie kleiner als die Magic Points ist, dann ist nicht genuf Magie für den Angriff übrig
+        //     else if (this.magic < this.skills[index].magicPoints) {
+        //         return `not enough magic, cannot launch attack!`;
+        // }
+    }
+    getMagic() {
+        const magicPlus = 20;
+        this.magic += magicPlus;
+        return `${this.name} got ${magicPlus} magic back`;
     }
 }
-
 
 // Pokemon
 
 let pikachu = new Pokemon("pikachu", 120, 80);
 let bulbasaur = new Pokemon("bulbasaur", 95, 105);
 
-
-
 class AttackSkill {
-    constructor(name,damage,kT){
+    constructor(name, damage, magicPoints) {
         this.name = name;
         this.damage = damage;
-        this.kT = kT;
+        this.magicPoints = magicPoints;
     }
 }
 
@@ -74,31 +85,31 @@ console.log(pikachu);
 console.log(pikachu.learnAttackSkill(thunder));
 console.log(pikachu);
 
-console.log("______________________________");
-
 // Bulbasaur Attacks
 
-let poisonSeed = new AttackSkill ("poison seed", 20, 20);
-let razorLeaf = new AttackSkill ("razor leaf", 50, 30)
+let poisonSeed = new AttackSkill("poison seed", 20, 20);
+let razorLeaf = new AttackSkill("razor leaf", 50, 30);
 console.log(bulbasaur.skills);
 console.log(bulbasaur.learnAttackSkill(poisonSeed));
 console.log(bulbasaur);
 console.log(bulbasaur.learnAttackSkill(razorLeaf));
 console.log(bulbasaur);
 
-console.log("-----------------------------------");
 // Fight
+console.log("-------------Battle-----------------");
+
 console.log(pikachu.showStatus());
 console.log(bulbasaur.showStatus());
-console.log("------------------------------------");
+
 console.log(pikachu.attack(0, bulbasaur));
 console.log(bulbasaur.attack(0, pikachu));
-console.log("_________________________________");
+
 console.log(pikachu.showStatus());
 console.log(bulbasaur.showStatus());
-console.log("_________________________________");
+
 console.log(pikachu.attack(0, bulbasaur));
 console.log(pikachu.attack(0, bulbasaur));
 console.log(pikachu.getMagic());
+
 console.log(pikachu.attack(0, bulbasaur));
 console.log(bulbasaur.attack(0, pikachu));
